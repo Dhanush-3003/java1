333b57bccb4b64238750c43efa737a7bf4ff394e
Dhanush-3003 <dhanushgowdaise2025@gmail.com>
2025-11-02 12:59:34 +0530

    Update f1.java

--- f1.java
+++ f1.java
@@ -2,5 +2,6 @@ class f1{
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("hiii");
+        System.out.println("welcome");
     } 
 }
